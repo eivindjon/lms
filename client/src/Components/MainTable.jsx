880190459eb1475deff81230e4cf4f5003e0bec5
eivindjon <eivind.b.jonassen@gmai.com>
2021-09-21 19:56:45 +0200

    proper table

--- client/src/Components/MainTable.jsx
+++ client/src/Components/MainTable.jsx
@@ -1,40 +1,58 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Axios from "axios";
-
+import { Table, Container } from "react-bootstrap";
 
 function MainTable() {
-  const [id, setId] = useState(0)
-  const [fornavn, setFornavn] = useState("")
-  const [etternavn, setEtternavn] = useState("")
+  const [studentList, setStudentList] = useState([]);
+  // let absenceList = [];
 
-  const [studentList, setStudentList] = useState([])
-  
-  function getStudents () {
-      Axios.get(`http://localhost:3001/getstudents`)
-        .then(res => {
-          const students = res.data;
-          setStudentList(students);
-        })
+  // Gets students from database on load (using useEffect)
+  function getStudents() {
+    Axios.get(`http://localhost:3001/getstudents`).then((res) => {
+      const students = res.data;
+      setStudentList(students);
+    });
   }
-
+  // Making the request to get students from db only ONCE. When render is complete. Instead of ComponentDidMount();
   useEffect(() => {
-    getStudents()
-    console.log("effect");
-  }, [])
+    getStudents();
+    console.log(studentList);
+    // eslint-disable-next-line
+  }, []);
+
+  // function addToAbcenseList(id) {
+  //   absenceList.push(id);
+  //   console.log(id, " pushed to absenceList");
+  // }
 
   return (
     <>
-      {studentList.map((val, key) => {
-          return (
-              <div>
-                <h3>ID: {val.id}</h3>
-                <h3>Fornavn: {val.fornavn}</h3>
-                <h3>Etternavn: {val.etternavn}</h3>
-              </div>)})
-      }
-    
+      <Container>
+        <Table striped bordered hover className="mt-5">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Fornavn</th>
+              <th>Etternavn</th>
+              <th>Valg</th>
+            </tr>
+          </thead>
+          <tbody>
+            {studentList.map((val, key) => {
+              return (
+                <tr>
+                  <td>{val.id}</td>
+                  <td>{val.fornavn}</td>
+                  <td>{val.etternavn}</td>
+                  <td>SE FRAVÆR</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </Table>
+      </Container>
     </>
-  
-  )};
+  );
+}
 
 export default MainTable;
