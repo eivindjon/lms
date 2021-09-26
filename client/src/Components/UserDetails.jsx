0419690d998ce1851ce2ef96395f2a8aa2d58ff8
eivindjon <eivind.b.jonassen@gmail.com>
2021-09-26 10:32:25 +0200

    fikse på userdetails

--- client/src/Components/UserDetails.jsx
+++ client/src/Components/UserDetails.jsx
@@ -1,20 +1,22 @@
 import React, { useState, useEffect } from "react";
 import Axios from "axios";
 import { Table, Container, Button } from "react-bootstrap";
+import { useLocation } from 'react-router-dom'
 
 function UserDetails() {
   const [absenceList, setAbsenceList] = useState([]);
 
   // Gets students from database on load (using useEffect)
-  function getStudentAbsense(id) {
-    const id = 
-    Axios.get(`http://localhost:3001/getstudents/`).then((res) => {
-      const students = res.data;
-      setStudentList(students);
+  function getStudentAbsense() {
+    const id = useLocation();
+    Axios.get(`http://localhost:3001/getstudents/`+id.location.pathname).then((res) => {
+      const absense = res.data;
+      setAbsenceList(absense);
     });
   }
   // Making the request to get students from db only ONCE. When render is complete. Instead of ComponentDidMount();
   useEffect(() => {
+
     getStudentAbsense();
     // eslint-disable-next-line
   }, []);
@@ -23,7 +25,6 @@ function UserDetails() {
 
   function handleClick(event) {
     console.log(event.target.id);
-    event.target.disabled = true;
   }
   return (
     <>
@@ -38,19 +39,18 @@ function UserDetails() {
             </tr>
           </thead>
           <tbody>
-            {studentList.map((students) => {
+            {absenceList.map((fravær) => {
               return (
-                <tr key={students.id}>
-                  <td>{students.id}</td>
-                  <td>{students.fornavn}</td>
-                  <td>{students.etternavn}</td>
+                <tr key={fravær.id}>
+                  <td>{fravær.dato}</td>
+                  <td>{fravær.fornavn}</td>
+                  <td>{fravær.etternavn}</td>
                   <td>
                     <Button
                       id={students.id}
                       onClick={handleClick}
-                      disabled={false}
                     >
-                      Borte!
+                      Endre
                     </Button>
                   </td>
                 </tr>
