8596a45b1f75b35f84ab0f172457f1152a7c43cc
eivindjon <eivind.b.jonassen@gmail.com>
2021-10-11 22:39:27 +0200

    work on oversikt ja

--- client/src/Components/Summary.jsx
+++ client/src/Components/Summary.jsx
@@ -1,31 +1,72 @@
 import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
 
 const Summary = (props) => {
-    const months =["Januar", "Februar", "Mars", "April", "Mai", "Juni", "Juli", "August", "September", "Oktober", "November", "Desember"]
-    const data = props.data;
-    const mergedMonthsData = data.reduce(function(result, field, index) {
-        result[months[index]] = field;
-        return result;
-      }, {})
-    const totalAbsent = data.reduce((a, b) => a + b, 0)
-    console.log("total days absent: ", totalAbsent)
+  const months = [
+    "Januar",
+    "Februar",
+    "Mars",
+    "April",
+    "Mai",
+    "Juni",
+    "Juli",
+    "August",
+    "September",
+    "Oktober",
+    "November",
+    "Desember",
+  ];
+  const data = props.data;
+  const mergedMonthsData = data.reduce(function (result, field, index) {
+    result[months[index]] = field;
+    return result;
+  }, {});
+  const totalAbsent = data.reduce((a, b) => a + b, 0);
+  console.log("total days absent: ", totalAbsent);
   return (
-  <div>
+    <Container>
       <h3>Antall fraværsdager hittil: {totalAbsent} </h3>
-      <p>Januar: {mergedMonthsData.Januar}</p>
-      <p>Februar: {mergedMonthsData.Februar}</p>
-      <p>Mars: {mergedMonthsData.Mars}</p>
-      <p>April: {mergedMonthsData.April}</p>
-      <p>Mai: {mergedMonthsData.Mai}</p>
-      <p>Juni: {mergedMonthsData.Juni}</p>
-      <p>Juli: {mergedMonthsData.Juli}</p>
-      <p>August: {mergedMonthsData.August}</p>
-      <p>September: {mergedMonthsData.September}</p>
-      <p>Oktober: {mergedMonthsData.Oktober}</p>
-      <p>November: {mergedMonthsData.November}</p>
-      <p>Desember: {mergedMonthsData.Desember}</p>
-  
-  </div>
+      <Row>
+        <Col>
+          <p>Januar: {mergedMonthsData.Januar}</p>
+        </Col>
+        <Col>
+          <p>Februar: {mergedMonthsData.Februar}</p>
+        </Col>
+        <Col>
+          <p>Mars: {mergedMonthsData.Mars}</p>
+        </Col>
+        <Col>
+          <p>April: {mergedMonthsData.April}</p>
+        </Col>
+        <Col>
+          <p>Mai: {mergedMonthsData.Mai}</p>
+        </Col>
+        <Col>
+          <p>Juni: {mergedMonthsData.Juni}</p>
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <p>Juli: {mergedMonthsData.Juli}</p>
+        </Col>
+        <Col>
+          <p>August: {mergedMonthsData.August}</p>
+        </Col>
+        <Col>
+          <p>September: {mergedMonthsData.September}</p>
+        </Col>
+        <Col>
+          <p>Oktober: {mergedMonthsData.Oktober}</p>
+        </Col>
+        <Col>
+          <p>November: {mergedMonthsData.November}</p>
+        </Col>
+        <Col>
+          <p>Desember: {mergedMonthsData.Desember}</p>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
