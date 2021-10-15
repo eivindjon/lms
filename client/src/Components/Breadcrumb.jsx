f1247fcd4ccfe3ba6c55333bea241688985eea19
eivindjon <eivind.b.jonassen@gmai.com>
2021-10-15 14:14:30 +0200

    ..

---
+++ client/src/Components/Breadcrumb.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Breadcrumb, Container } from "react-bootstrap";
+
+const navigationCrumb = (props) => {
+  const pathname = window.location.pathname.split("/");
+  console.log(pathname);
+
+  // TODO: make it createt breadcrumb-items dynamically. Recieve props with navigation.
+  return (
+    <>
+      <Container className="mt-4">
+        <Breadcrumb>
+          <Breadcrumb.Item href="../">Hjem</Breadcrumb.Item>
+          <Breadcrumb.Item active>Detaljer</Breadcrumb.Item>
+        </Breadcrumb>
+      </Container>
+    </>
+  );
+};
+
+export default navigationCrumb;
