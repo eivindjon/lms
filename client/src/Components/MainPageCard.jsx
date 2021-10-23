b87d74d85fdffb67d4d4dca8bed9f160fe89ab80
eivindjon <eivind.b.jonassen@gmail.com>
2021-10-23 12:37:57 +0200

    REVAMP STATIC LESSONS

--- client/src/Components/MainPageCard.jsx
+++ client/src/Components/MainPageCard.jsx
@@ -1,10 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect} from "react";
 import { Card, Button } from "react-bootstrap";
 import { ArrowRight, ArrowLeft } from "react-bootstrap-icons";
+import Axios from "axios";
 
 function MainPageCard(props) {
   const [day, setDay] = useState(new Date());
-  const [weekday, setWeekday] = useState("Unknown");
+  const [weekday, setWeekday] = useState(day.getDay());
+  const [staticLessons, setStaticLessons] = useState(false);
+  const [lessons, setLessons] = useState([{}]);
+
+  const dailyLessons = {
+    0 : [],
+    1 : ["Matematikk 9E", "Matematikk 8D", "Samfunnsfag 9E"],
+    2 : ["Naturfag 9E", "Matematikk 9E", "Samfunnsfag 9E", "Programmering"],
+    3 : ["Naturfag 8D", "Matematikk 8D"],
+    4 : ["Naturfag 9E", "Fysak"],
+    5 : ["Matematikk 9E", "Samfunnsfag 9E", "Matematikk 8D"],
+    6 : []
+  }
+
+  const renderLessons = (object, weekday) => {
+      
+    
+      console.log(object[weekday])
+  }
 
   const capper = (date) => {
     const options = {
@@ -18,51 +37,80 @@ function MainPageCard(props) {
     return capitals;
   };
 
+
   const addDays = (date, days) => {
     const result = new Date(date);
     result.setDate(result.getDate() + days);
+    setWeekday(result.getDay());
     return result;
   };
 
   const subDays = (date, days) => {
     const result = new Date(date);
     result.setDate(result.getDate() - days);
+    setWeekday(result.getDay());
     return result;
   };
 
   const handleClickNext = () => {
-    console.log("Nextday");
-    setDay(addDays(day, 1));
+    if (weekday === 5) {
+      setDay(addDays(day, 3));
+    } else {
+      setDay(addDays(day, 1));
+    }
   };
 
   const handleClickPrev = () => {
-    console.log("Prevday");
-    setDay(subDays(day, 1));
+    if (weekday === 1) {
+      setDay(subDays(day, 3));
+    } else {
+      setDay(subDays(day, 1));
+      console.log(day.toLocaleString("en-CA").substr(0, 10));
+    }
   };
 
+  
+
+  useEffect(() => {
+
+    console.log(day, "->Day - Has changed");
+
+    function getLessons() {
+      Axios.get(
+        `http://localhost:3001/getlessons/${day
+          .toLocaleString("en-CA")
+          .substr(0, 10)}`
+      ).then((res) => {
+        const result = res;
+          setLessons(result);
+        }
+      );
+    }
+    getLessons();
+    
+  }, [day]); // <-- here put the parameter to listen
+
+  //Querys if there are any planned lessons
+  //if result.length < 1
+  // setStatic(true)
+  // else setLessons(result)
   return (
     <Card>
       <Card.Header as="h5">
         {capper(day)}
         <div className="float-end">
-          <Button  onClick={handleClickPrev}>
-            <ArrowLeft/>
+          <Button onClick={handleClickPrev}>
+            <ArrowLeft />
           </Button>
-          <Button className="ms-2" onClick={handleClickNext}><ArrowRight/>
+          <Button className="ms-2" onClick={handleClickNext}>
+            <ArrowRight />
           </Button>
         </div>
       </Card.Header>
       <Card.Body>
-        <Card variant="success">
-          <Card.Header as="h7">
-            Samfunnsfag
-          </Card.Header>
-          <Card.Body>
-              <Card.Text>
-                YOYOYO
-              </Card.Text>
-            </Card.Body>
-        </Card>
+        
+        {/* map the lessons into each its own cards. */}
+        {/* <LessonCard subject={lesson.subject} description={lesson.description} notes={lesson.notes}/> */}
       </Card.Body>
     </Card>
   );
